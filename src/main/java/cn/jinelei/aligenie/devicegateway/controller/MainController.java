1081f4bb80454f4795f076a4e43b0991f56ef0f1
jinelei <jinelei@163.com>
2018-09-30 17:37:22 +0800

    add output

--- src/main/java/cn/jinelei/aligenie/devicegateway/controller/MainController.java
+++ src/main/java/cn/jinelei/aligenie/devicegateway/controller/MainController.java
@@ -3,6 +3,7 @@ package cn.jinelei.aligenie.devicegateway.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +12,16 @@ public class MainController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         LOGGER.debug("request index");
         return "index";
     }
 
     @RequestMapping("/gateway")
-    public String gateway(){
-        LOGGER.debug("request gateway");
+    public String gateway(@RequestBody String body) {
+        LOGGER.debug("request gateway: ");
+        LOGGER.debug(body);
+        System.out.println(body);
         return "gateway";
     }
 
